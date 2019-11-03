39f2cdd992fb6a5b6678f1fd00f4ad18863144e7
肖俊俏 <oncwnuBPra-J9qujju1GsNUkBJuM@git.weixin.qq.com>
2019-11-03 09:43:28 +0800

    第九次提交 第29课代码

--- cloudfunctions/remove/index.js
+++ cloudfunctions/remove/index.js
@@ -8,11 +8,16 @@ const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
   try {
-    return await db.collection('test').where({
+    return await db.collection('test1').where({
       _openid: 'oK1SB4tF0GrSzxSZEKWCQTaY3DOw'   //  删除done字段为true的所有文档
     }).remove()
-    console.log("success")
+    
   } catch (e) {
     console.error(e)
+  }
+  console.log(JSON.stringify(event) )
+  console.log(JSON.stringify(event).errNsg)
+  if (JSON.stringify(event).errNsg=="cloud.callFunction:ok"){
+    return JSON.stringify(event.result)
   }
 }
